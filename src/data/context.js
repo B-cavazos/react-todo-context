cd85618b953e377bb060446f762d776d77bff962
Brittney <brittney.cavazos@gmail.com>
2021-04-27 16:21:29 -0700

    feature(context)added data to context.js

--- src/data/context.js
+++ src/data/context.js
@@ -1,1 +1,45 @@
-file to hold states and such
+import React, { createContext, useState, useEffect } from 'react';
+
+const AppContext = createContext();
+
+const AppContextProvider = ({ children }) => {
+    /* state for input form */
+    const [inputText, setInputText] = useState('');
+
+    /* state for todo items */
+    const [todos,setTodos] = useState([{
+        id: null,
+        task:'',
+        completed: false,
+
+    }]); 
+
+    /* inputTextHandler */
+    const inputTextHandler = (e) => {
+        e.preventDefault();
+        console.log(e.target.value);
+        setInputText(e.target.value);
+    };
+
+    /* submitHandler */
+    const submitHandler = (e) => {
+        e.preventDefault();
+        setTodos(...todos, {
+            id: Date.now(),
+            task: inputText, 
+            completed: false
+        });
+        setInputText('');
+    };
+
+/* useEffect for button? */
+
+
+  return (
+    <AppContext.Provider value={{inputTextHandler, submitHandler}}>
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+export { AppContext, AppContextProvider };
